c40ced164f17e7b7cad64f8f776168604e6d03b5
Brianne Hostutler <briannehostutler@gmail.com>
2016-04-12 20:06:31 -0400

    added switch

--- liri.js
+++ liri.js
@@ -12,26 +12,47 @@
 // do-what-it-says
 
 // node liri.js my-tweets:
+var fs = require('fs');
+
+var action = process.argv[2];
+
+switch(action) {
+    case 'my-tweets': myTweets(); break;
+    case 'spotify-this-song': spotifyThisSong(); break;
+    case 'movie-this': movieThis(); break;
+    case 'do-what-it-says': doWhatItSays(); break;
+};
 
 
-var fs = require('fs'); //reads and writes files
+function myTweets() {
 
-fs.readFile(".gitignore/keys.js", "utf8", function(error, data) {
-    var keys= data;
-    console.log(keys);
-});
+	var fs = require('fs'); //reads and writes files
 
+	fs.readFile(".gitignore/keys.js", "utf8", function(error, data) {
+	    var keys= data;
+	    console.log(keys);
+	});
+}
 
-var request = require('request');
+function spotifyThisSong() {
+}
 
-movie = process.argv[2];
+function movieThis() {
 
-request('http://www.omdbapi.com/?t='+movie+'&y=&plot=short&r=json"', function (error, response, body) {
-  if (!error && response.statusCode == 200) { //200 = successful request
-    console.log(body); // Show the text in the terminal
-	}
-});
+	var request = require('request');
 
+	movie = process.argv[3];
+
+	request('http://www.omdbapi.com/?t='+movie+'&y=&plot=short&r=json"', function (error, response, body) {
+	  if (!error && response.statusCode == 200) { //200 = successful request
+	    console.log(body); // Show the text in the terminal
+		}
+	});
+
+};
+
+function doWhatItSays() {
+};
 
 // var request = require('request');
 
